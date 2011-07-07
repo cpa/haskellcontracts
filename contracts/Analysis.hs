61a33d686f042e8146042b3d81cff39b2ff2a9e5
Charles-Pierre Astolfi <t-chasto@microsoft.com>
2011-07-07 17:10:19 +0100

    mend

---
+++ contracts/Analysis.hs
@@ -0,0 +1,32 @@
+module Analysis where
+
+import Data.Graph hiding (scc)
+import Data.Graph.SCC 
+import Data.List (nub)
+import Haskell
+
+graphFromProgram :: [DefGeneral Expression] -> (Graph,Vertex -> (Variable, Variable, [Variable]),Variable -> Maybe Vertex)
+graphFromProgram p = graphFromEdges [ go d | d <- p, case d of Def _ -> True ; _ -> False ]
+  where go (Def (Let f xs e)) = (f,f,freeVars f (xs++dataV) e)
+        go (Def (LetCase f xs e pes)) = (f,f,concatMap (\(p,e) -> freeVars f (xs++dataV++p) e) pes)
+        dataV = dataVars =<< [ d | d <- p, case d of DataType _ -> True ; _ -> False ]
+        
+dataVars :: DefGeneral t -> [Variable]
+dataVars (DataType (Data _ vacs)) = map fst3 vacs
+  where fst3 (a,_,_) = a
+
+freeVars :: Variable -> [Variable] -> Expression -> [Variable]
+freeVars f xs BAD = []
+freeVars f xs (Var v) = if  v `elem` xs then [] else [v]
+freeVars f xs (App e1 e2) = freeVars f xs e1 ++ freeVars f xs e2
+freeVars f xs (FullApp g es) = g : ((freeVars f xs) =<< es)
+freeVars f xs (Sat _ _) = undefined -- yet
+freeVars f xs (CF e) = freeVars f xs e
+
+checkOrder :: [DefGeneral Expression] -> [[Variable]]
+checkOrder p = reverse $ map nub topOrder
+  where (g,a,b) = graphFromProgram p
+        (e,f) = scc $ g
+        topOrderAux = map (\(b,as) -> (map a as)) e
+        topOrder = map (map (\(_,f,fs) -> f)) topOrderAux
+        
---
+++ contracts/Check.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import Analysis (checkOrder)
+import Parser hiding (main)
+import Translation
+import Control.Monad
+import FOL (toTPTP,simplify)
+import System.Environment
+import System.IO
+import Haskell
+import System.Process
+import System.Directory (removeFile)
+import Control.Applicative
+
+main = do
+  f:_ <- getArgs
+  res <- checkFile f
+  if res 
+    then putStrLn $ "All the contracts in " ++ f ++ "hold."
+    else putStrLn $ "There's at least one contract in " ++ f ++ "that took more than 10 sec to prove."
+  
+checkFile f = do
+  s <- readFile f
+  let prog = appify $ haskell $ lexer s
+      order = checkOrder prog
+  res <- sequence $ go order prog []
+  return $ and res
+  where go [] prog checkedDefs = []
+        go (fs:fss) prog checkedDefs = check prog fs checkedDefs : go fss prog (fs++checkedDefs)
+  
+check :: Program -> [Variable] -> [Variable] -> IO Bool
+check prog [] _ = error "Should not happen"
+check prog [f] checkedDefs = do
+  let hasBeenChecked checkedDefs (Def (Let f _ _)) = f `elem` checkedDefs
+      hasBeenChecked checkedDefs (Def (LetCase f _ _ _)) = f `elem` checkedDefs
+      hasBeenChecked _ _ = True
+      safeSubset prog checkedDefs = filter (hasBeenChecked checkedDefs) prog
+      tptpTheory = trans (safeSubset prog checkedDefs) f >>= simplify >>= toTPTP
+      tmpFile = "tmp.tptp"
+  putStrLn $ "Checking " ++ f ++ "..."
+  writeFile tmpFile tptpTheory
+  res <- isUnsat . last . lines <$> readProcess "./equinox" [tmpFile] ""
+  removeFile tmpFile
+  return res
+    where isUnsat s = s == "+++ RESULT: Unsatisfiable"
+  
+check prog _ _ = error "Not implemented yet"
