27a67f9b339f8bfdac5717189e3ce6310a39e83b
Charles-Pierre Astolfi <t-chasto@microsoft.com>
2011-07-08 15:11:06 +0100

    mend

--- contracts/Check.hs
+++ contracts/Check.hs
@@ -13,22 +13,30 @@ import System.Process
 import System.Directory (removeFile)
 import Control.Applicative
 
+data Conf = Conf { timeLimit :: Int
+                 , printTPTP :: Bool } 
+
+conf flags = go flags (Conf 10 False)
+  where go ("-t":n:flags) cfg = go flags (cfg {timeLimit=read n :: Int})
+        go ("-p":flags)   cfg = go flags (cfg {printTPTP=True})
+        go (_:flags)      cfg = go flags cfg
+
 main = do
-  f:_ <- getArgs
-  res <- checkFile f
+  f:flags <- getArgs
+  res <- checkFile f (conf flags)
   if res 
     then putStrLn $ f ++ ": all the contracts hold."
     else putStrLn $ "There's at least one contract in " ++ f ++ " that took more than 5 sec to prove."
   
-checkFile f = do
+checkFile f cfg = do
   s <- readFile f
   let prog = appify $ haskell $ lexer s
       order = checkOrder prog
-  system "ulimit -t 20"
-  res <- sequence $ go prog [] order
+  system $ "ulimit -t " ++ show (timeLimit cfg)
+  res <- sequence $ go prog [] cfg order
   return $ and res
-  where go prog checkedDefs [] = []
-        go prog checkedDefs (fs:fss) = check prog fs checkedDefs : go prog (fs++checkedDefs) fss
+  where go prog checkedDefs cfg [] = []
+        go prog checkedDefs cfg (fs:fss) = check prog fs cfg checkedDefs : go prog (fs++checkedDefs) cfg fss
   
 hasBeenChecked checkedDefs (Def (Let f _ _)) = f `elem` checkedDefs
 hasBeenChecked checkedDefs (Def (LetCase f _ _ _)) = f `elem` checkedDefs
@@ -38,9 +46,9 @@ hasNoContract f prog = and $ (flip map) prog $ \d -> case d of
   ContSat (Satisfies g _) -> f /= g
   _ -> True
 
-check :: Program -> [Variable] -> [Variable] -> IO Bool
-check prog [] _ = error "Should not happen"
-check prog [f] checkedDefs | f `hasNoContract` prog = return True
+check :: Program -> [Variable] -> Conf -> [Variable] -> IO Bool
+check prog [] cfg _ = error "There should be at least one definition!"
+check prog [f] cfg checkedDefs | f `hasNoContract` prog = return True
                            | otherwise = do
   let safeSubset prog checkedDefs = filter (hasBeenChecked (f:checkedDefs)) prog
       tptpTheory = trans (safeSubset prog checkedDefs) [f] >>= simplify >>= toTPTP
@@ -51,10 +59,13 @@ check prog [f] checkedDefs | f `hasNoContract` prog = return True
   removeFile tmpFile
   when res $ 
     putStrLn "\tOK!"
+  when (printTPTP cfg) $ do
+    putStrLn tptpTheory
+    putStrLn "\n==============================================================\n"
   return res
     where isUnsat s = "Unsatisfiable" `elem` tails s
   
-check prog fs checkedDefs = do
+check prog fs cfg checkedDefs = do
   let safeSubset prog checkedDefs = filter (hasBeenChecked (fs++checkedDefs)) prog
       tptpTheory = trans (safeSubset prog checkedDefs) fs >>= simplify >>= toTPTP
       tmpFile = "tmp.tptp"
@@ -64,6 +75,9 @@ check prog fs checkedDefs = do
   removeFile tmpFile
   when res $
     putStrLn "\tOK!"
+  when (printTPTP cfg) $ do
+    putStrLn tptpTheory
+    putStrLn "\n==============================================================\n"
   return res
     where isUnsat s = "Unsatisfiable" `elem` tails s
           showfs [] = ""
