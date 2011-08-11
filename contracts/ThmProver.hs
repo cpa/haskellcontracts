4be3090be255a10cf66414937fc92a3b94cd699f
Charles-Pierre Astolfi <t-chasto@microsoft.com>
2011-08-11 15:21:24 +0100

    multiple engines

---
+++ contracts/ThmProver.hs
@@ -0,0 +1,14 @@
+module ThmProver where
+
+import Data.List
+
+data ThmProverOpt = T { path :: FilePath
+                      , opts :: String
+                      , unsat  :: String -> Bool }
+
+type ThmProver = (String,ThmProverOpt)
+                    
+provers :: [ThmProver]
+provers = [ ("equinox", T "equinox" "" ("Unsatisfiable" `isInfixOf`))
+          , ("SPASS", T "SPASS" "-PProblem=0 -PGiven=0 -TPTP" ("Proof found" `isInfixOf`))
+          , ("vampire", T "vampire_lin64" "--mode casc --input_file" ("Refutation" `isInfixOf`)) ]
