81811221adc85689683eee48bca9e49337c6a3ee
Nathan Collins <nathan.collins@gmail.com>
2011-11-16 23:34:47 +0000

    Rewrote the SCC code to do almost the same thing ...
    But now I'm prepared to make it do the right thing, which is
    allow for minimal theory generation.  The major change, still
    incomplete, is to include 'DataType's in the graph.

--- contracts/Analysis.hs
+++ contracts/Analysis.hs
@@ -1,50 +1,101 @@
 module Analysis where
 
-import Data.Graph hiding (scc)
-import Data.Graph.SCC (scc)
+-- http://hackage.haskell.org/packages/archive/containers/0.4.1.0/doc/html/Data-Graph.html#v:graphFromEdges
+import Data.Graph (graphFromEdges,Graph,Vertex,SCC(..))
+-- http://hackage.haskell.org/packages/archive/GraphSCC/1.0.2/doc/html/Data-Graph-SCC.html
+import Data.Graph.SCC (sccGraph)
 import Data.Maybe (fromJust)
 import Data.List (nub)
+import qualified Data.Map as M
+
 import Haskell
 import Parser
 
+{- Some simple graph examples to try: -}
+--
+-- 0   3
+--  \ /|
+--   2 |
+--  / \|
+-- 1   4
+--
+-- directed left to right, with double edges between 3 and 4.
+edges = [(0,0,[2]),(1,1,[2]),(2,2,[3,4]),(3,3,[4]),(4,4,[3])]
+fstOf3 (x,_,_) = x
+g = fstOf3 $ graphFromEdges $ edges
+-- Quotient by SCC relation.
+qEdges = sccGraph g
+q = fstOf3 $ graphFromEdges $ qEdges
+-- SCCs and their dependencies.
 
--- takes a program and returns a graph where nodes are functions
--- and a is connected to b iff b's definition uses a
-graphFromProgram :: Program -> (Graph,Vertex -> (Variable, Variable, [Variable]),Variable -> Maybe Vertex)
-graphFromProgram p = graphFromEdges [go d | d@(Def _) <- p]
-  where go (Def (Let f xs e)) = (f,f,freeVars (xs++dataV) e ++ varsInCont p f)
-        go (Def (LetCase f xs e pes)) = (f,f,varsInCont p f ++ (concatMap (\((_,ys),e) -> freeVars (xs++dataV++ys) e) $ ((undefined,[]),e):pes))
---        go (ContSat (Satisfies f c))  = (f,f,contVars f dataV c)
-        dataV = dataVars =<< [d | d@(DataType _) <- p]
+instance Show a => Show (SCC a) where
+  show (AcyclicSCC v) = "A "++show v
+  show (CyclicSCC vs) = "C "++show vs
 
--- finds the contract for f in p and returns the variables in it
--- Consider this example: 
--- f x = x
--- f ::: {x: eq x 0} -> {y: eq y 0}
--- f doesn't use any function so it has nothing pointing to it in the graph
--- but still, we need to include eq's definition in the theory!
-varsInCont p f = contVars f [] =<< [c | ContSat (Satisfies x c) <- p, x == f]
+scc = undefined
+
+-- 1. Build a graph G of dependencies, with 'Name's for nodes, where
+-- 'n1' points to 'n2' if 'n1' depends on 'n2', which in turn happens
+-- iff 'n1's definition mentions 'n2', 'n1's contract mentions 'n2',
+-- or 'n2' is a 'DataType' and 'n1' mentions one of it's term
+-- constructors.
+--
+-- 2. Compute the quotient graph Q of strongly connected components
+-- (SCCs) of G, and topologically sort Q.
+--
+-- 3. Return the SCC's of Q, along with there transitive dependencies,
+-- in topological order.  Here "topological order" means dependencies
+-- come before things that depend on them.
+mkGAdjacency defs = [(f,f, map substType ds) | (f,ds) <- map adj funDefs]
+ where
+  funDefs = [d | Def d <- defs]
+  -- Term constructor types.
+  types = [(c,t) | DataType (Data t cs) <- defs, (c,_,_) <- cs]
+  -- Substitute a term con for its type.
+  substType :: Name -> Name
+  substType c = case lookup c types of
+                  Nothing -> c
+                  Just t  -> t
+  -- Adjacency of a single function, but with term constructors still
+  -- to be substituted.
+  adj (Let f xs e)         = (f, freeVars xs e ++ allContFreeVars f)
+  adj (LetCase f xs e pes) = (f, freeVars xs e ++ allContFreeVars f ++
+                                       concat [freeVars (xs++ysi) ei
+                                              | ((_,ysi),ei) <- pes])
+  -- Free vars for all contracts in program for 'f'.
+  allContFreeVars f = contFreeVars =<< [c | ContSat (Satisfies f' c) <- defs, f' == f]
+
+-- TODO:
+--
+-- 1. make sure all data types have nodes, since the Data.Graph
+-- ignores nodes that in adjs that don't have an adj list themselves?
+--
+-- 2. consider using (DefGeneral,Name,[Name]) instead of
+-- (Name,Name,[Name]) for the Graph.  see what the code in Check.hs
+-- uses us for first.  In any case, we have to look up all the
+-- contracts, since we don't include them in the graph (we'd lose
+-- uniqueness of keys).
+
+graphFromProgram = graphFromEdges . mkGAdjacency
 
 -- returns the variables used in a contract
-contVars f xs (Arr x c1 c2) = contVars f xs c1 ++ contVars f xs c2
-contVars f xs (Pred x e)     = freeVars (x:xs) e
-contVars f xs (And c1 c2)    = contVars f xs c1 ++ contVars f xs c2
-contVars f xs (Or c1 c2)     = contVars f xs c1 ++ contVars f xs c2
-contVars f xs CF             = []
-contVars f xs Any            = []
-        
-dataVars :: DefGeneral -> [Variable]
-dataVars (DataType (Data _ vacs)) = map fst3 vacs
-  where fst3 (a,_,_) = a
+contFreeVars c = go [] c where
+  go xs (Arr mx c1 c2) = go xs' c1 ++ go xs' c2 where xs' = maybe xs (:xs) mx
+  go xs (Pred x e)    = freeVars (x:xs) e
+  go xs (And c1 c2)   = go xs c1 ++ go xs c2
+  go xs (Or c1 c2)    = go xs c1 ++ go xs c2
+  go xs CF            = []
+  go xs Any           = []
 
 -- | Return the list of free variables in an expression.
 --
 -- 'xs' is a list of bound variables.
 
--- XXX, BUG???: this function returns constructor names as well,
--- although these should never be considered free.  The old code did
--- this, so I preserved this behavior when I updated it, but I did not
--- check that it was necessary --- NC.
+-- NB: this function returns constructor names as well, although these
+-- should never be considered free.  The old code did this, so I
+-- preserved this behavior when I updated it, but I did not check that
+-- it was necessary --- NC. UPDATE: yes, we want the constructor
+-- names, so we can deduce what types a term depends on.
 freeVars :: [Name] -> Expression -> [Name]
 freeVars xs (Named v) = if (getName v) `elem` xs then [] else [getName v]
 freeVars xs (e1 :@: e2) = freeVars xs e1 ++ freeVars xs e2
