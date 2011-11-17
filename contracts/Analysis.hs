1e6d1b8727a8211a65a6096f400106ca679a1df7
Nathan Collins <nathan.collins@gmail.com>
2011-11-17 16:18:45 +0000

    Checkpoint: Implemented new version order-to-check-things code.
     This version is supposed to only include a minimal set of definitions
     and contracts.  The old version would include everything that came before
    in the topo sorted graph, even if unrelated, and all datatype definitions.
    
    This version only includes the dependencies of the defs to check.
    
    It's untested, because it's not integrated into the checking code yet.

--- contracts/Analysis.hs
+++ contracts/Analysis.hs
@@ -1,17 +1,36 @@
+-- Compute the defs that generate the theory to check.
+--
+-- 1. Build a graph G of dependencies, with function and datatype defs
+-- for nodes, and function and datype names for keys.  Here 'd1'
+-- points to 'd2' if 'd1' depends on 'd2', i.e. iff 'd1's definition
+-- mentions 'd2', 'd1's contract mentions 'd2', 'd1' has a predicate
+-- contract and 'd2' is 'Bool', or 'd2' is a 'DataType' and 'd1'
+-- mentions one of it's term constructors.
+--
+-- NB: the G does not include contracts. The main reason is that NC
+-- couldn't think of a good way to get unique keys for them, since we
+-- already key function defs by the function name.  So, the theory
+-- generation code has to look up the contracts for the functions it
+-- processes.
+--
+-- 2. Compute the quotient graph Q of strongly connected components
+-- (SCCs) of G. Return the defs in the SCC's of Q, along with there
+-- transitive dependencies, in topological order.  Here "topological
+-- order" means dependencies come before things that depend on them.
 module Analysis where
 
 -- http://hackage.haskell.org/packages/archive/containers/0.4.1.0/doc/html/Data-Graph.html#v:graphFromEdges
-import Data.Graph (graphFromEdges,Graph,Vertex,SCC(..))
+import qualified Data.Graph as G
 -- http://hackage.haskell.org/packages/archive/GraphSCC/1.0.2/doc/html/Data-Graph-SCC.html
 import Data.Graph.SCC (sccGraph)
-import Data.Maybe (fromJust)
-import Data.List (nub)
-import qualified Data.Map as M
+import Data.Maybe (fromJust,catMaybes)
+import Data.List (nub,delete)
 
 import Haskell
-import Parser
 
-{- Some simple graph examples to try: -}
+fstOf3 (x,_,_) = x
+
+{- Graph example to help understand Data.Graph functions. Try in GHCi:
 --
 -- 0   3
 --  \ /|
@@ -20,68 +39,101 @@ import Parser
 -- 1   4
 --
 -- directed left to right, with double edges between 3 and 4.
-edges = [(0,0,[2]),(1,1,[2]),(2,2,[3,4]),(3,3,[4]),(4,4,[3])]
-fstOf3 (x,_,_) = x
-g = fstOf3 $ graphFromEdges $ edges
+-}
+edges = [("0",0,[2]),("1",1,[2]),("2",2,[3,4]),("3",3,[4]),("4",4,[3])]
+g = fstOf3 $ G.graphFromEdges $ edges
 -- Quotient by SCC relation.
 qEdges = sccGraph g
-q = fstOf3 $ graphFromEdges $ qEdges
+q = fstOf3 $ G.graphFromEdges $ qEdges
 -- SCCs and their dependencies.
 
-instance Show a => Show (SCC a) where
-  show (AcyclicSCC v) = "A "++show v
-  show (CyclicSCC vs) = "C "++show vs
+instance Show a => Show (G.SCC a) where
+  show (G.AcyclicSCC v) = "A "++show v
+  show (G.CyclicSCC vs) = "C "++show vs
+{- End examples -}
 
-scc = undefined
-
--- 1. Build a graph G of dependencies, with 'Name's for nodes, where
--- 'n1' points to 'n2' if 'n1' depends on 'n2', which in turn happens
--- iff 'n1's definition mentions 'n2', 'n1's contract mentions 'n2',
--- or 'n2' is a 'DataType' and 'n1' mentions one of it's term
--- constructors.
---
--- 2. Compute the quotient graph Q of strongly connected components
--- (SCCs) of G, and topologically sort Q.
---
--- 3. Return the SCC's of Q, along with there transitive dependencies,
--- in topological order.  Here "topological order" means dependencies
--- come before things that depend on them.
-mkGAdjacency defs = [(f,f, map substType ds) | (f,ds) <- map adj funDefs]
+-- (1)
+adjacencies defs = funAdjs ++ typeAdjs
  where
-  funDefs = [d | Def d <- defs]
-  -- Term constructor types.
-  types = [(c,t) | DataType (Data t cs) <- defs, (c,_,_) <- cs]
-  -- Substitute a term con for its type.
-  substType :: Name -> Name
-  substType c = case lookup c types of
-                  Nothing -> c
-                  Just t  -> t
   -- Adjacency of a single function, but with term constructors still
   -- to be substituted.
-  adj (Let f xs e)         = (f, freeVars xs e ++ allContFreeVars f)
-  adj (LetCase f xs e pes) = (f, freeVars xs e ++ allContFreeVars f ++
-                                       concat [freeVars (xs++ysi) ei
-                                              | ((_,ysi),ei) <- pes])
-  -- Free vars for all contracts in program for 'f'.
-  allContFreeVars f = contFreeVars =<< [c | ContSat (Satisfies f' c) <- defs, f' == f]
+  adj d@(Def d') = (d,f,deps) where
+    (f,xs,e,mpes) = case d' of
+      Let     f xs e     -> (f,xs,e,Nothing)
+      LetCase f xs e pes -> (f,xs,e,Just pes)
+    -- Body/scrutinee and contract dependencies.
+    eDeps = freeVars xs e ++ allContFreeVars f where
+      allContFreeVars f = contFreeVars =<<
+                          [c | ContSat (Satisfies f' c) <- defs, f' == f]
+    -- Pattern dependencies.
+    pDeps = case mpes of
+      Nothing -> []
+      Just pes -> concat [freeVars (xs++ysi) ei | ((_,ysi),ei) <- pes]
+    deps = eDeps ++ pDeps
 
--- TODO:
---
--- 1. make sure all data types have nodes, since the Data.Graph
--- ignores nodes that in adjs that don't have an adj list themselves?
---
--- 2. consider using (DefGeneral,Name,[Name]) instead of
--- (Name,Name,[Name]) for the Graph.  see what the code in Check.hs
--- uses us for first.  In any case, we have to look up all the
--- contracts, since we don't include them in the graph (we'd lose
--- uniqueness of keys).
+  funAdjsWithCons = map adj [d | d@(Def _) <- defs]
+  -- Term constructor types.
+  con2Type = [(c,d) | d@(DataType (Data _ cs)) <- defs, (c,_,_) <- cs]
+  -- Replace term constructors with their types in dependencies.
+  funAdjs = map (\(d,f,ds) -> (d,f,map substType ds)) funAdjsWithCons where
+    -- Substitute a term con for its type.
+    substType :: Name -> Name
+    substType c = case lookup c con2Type of
+                    Nothing -> c
+                    Just (DataType (Data t _)) -> t
 
-graphFromProgram = graphFromEdges . mkGAdjacency
+  -- Add empty adjs for each type appearing in dependencies:
+  -- Data.Graph.graphFromEdges discards dependencies w/o an adjacency
+  -- list :P (Wouldn't it be better to just assume they have no out
+  -- edges?)
+  typeAdjs = [(d,t,[]) | d@(DataType (Data t _)) <- typeDeps] where
+    -- All dependencies, still with term constructors.
+    deps = nub $ concat $ [ds | (_,_,ds) <- funAdjsWithCons]
+    -- Types of all term constructors in deps.
+    typeDeps :: [DefGeneral]
+    typeDeps = nub $ catMaybes $ map (`lookup` con2Type) deps
+
+-- (2)
+--
+-- Compute pairs of '(checks, depends)', in topological order of the
+-- 'checks's, where 'depends' are the dependencies of the 'checks'.
+-- The 'checks' are a strongly connected component in the dependency
+-- graph.
+checkOrder :: Program -> [([DefGeneral],[DefGeneral])]
+checkOrder defs = checkDeps where
+  -- Dependency graph 'g'.
+  gAdjs = adjacencies defs
+  (g,gVertex2Adj,_) = G.graphFromEdges gAdjs
+
+  -- Compute SCC quotient graph of 'g'.
+  qAdjs :: [(G.SCC G.Vertex, Int, [Int])]
+  -- Reverse here to get topo order: sccGraph returns adjs in reverse
+  -- topo order.  The 'Vertex's here are from the 'g', the 'Int' keys
+  -- are new.
+  qAdjs = reverse . sccGraph $ g
+  (q,qVertex2Adj,qKey2MaybeVertex) = G.graphFromEdges qAdjs
+
+  checkDeps :: [([DefGeneral],[DefGeneral])]
+  checkDeps = [ (sccVertex2Defs v, sccVertex2Defs =<< ds)
+              | (v,ds) <- sccCheckDeps
+              ]
+   where
+    -- The '(checks,deps)' pairs of SCC vertices.
+    sccCheckDeps = [ (v,delete v (G.reachable q v)) -- 'delete' in case of self-edge.
+                   | (_,k,_) <-qAdjs,  Just v <- [qKey2MaybeVertex k]
+                   ]
+    -- Recover the definitions from an SCC vertex.  There are two levels
+    -- of indirection here, because each graph abtracts nodes via keys.
+    -- Here we follow the keys backwards to their nodes.
+    sccVertex2Defs :: G.Vertex -> [DefGeneral]
+    sccVertex2Defs = map (fstOf3 . gVertex2Adj) -- recover g nodes
+                   . G.flattenSCC               -- get list from SCC
+                   . fstOf3 . qVertex2Adj       -- recover q nodes
 
 -- returns the variables used in a contract
 contFreeVars c = go [] c where
   go xs (Arr mx c1 c2) = go xs' c1 ++ go xs' c2 where xs' = maybe xs (:xs) mx
-  go xs (Pred x e)    = freeVars (x:xs) e
+  go xs (Pred x e)    = "Bool":freeVars (x:xs) e -- All preds depend on 'Bool'
   go xs (And c1 c2)   = go xs c1 ++ go xs c2
   go xs (Or c1 c2)    = go xs c1 ++ go xs c2
   go xs CF            = []
@@ -100,20 +152,3 @@ freeVars :: [Name] -> Expression -> [Name]
 freeVars xs (Named v) = if (getName v) `elem` xs then [] else [getName v]
 freeVars xs (e1 :@: e2) = freeVars xs e1 ++ freeVars xs e2
 freeVars xs (FullApp g es) = (getName g) : ((freeVars xs) =<< es)
-
--- black magic.  returns a list of list of function names.  When
--- traversed, the list gives in order the set of definitions to check.
--- eg, the image p3 of the paper.pdf could give
--- [[f],[g],[h],[f,g,a],[b],[f,g,h,a,b,c]] 
-
--- because we check f,g,h first and we do some book-keeping to know
--- what has been or hasn't been checked yet, when checking [f,g,a] we
--- will only check a's contract assuming f's,g's contract hold (it has
--- been checked before)
--- all of this is done by Check.hs, not here.
-checkOrder :: Program -> [[Variable]]
-checkOrder p = reverse $ map nub topOrder
-  where (g,a,b) = graphFromProgram p
-        (e,f) = scc $ g
-        topOrderAux = map (\(b,as) -> (map a as)) e
-        topOrder = map (map (\(_,f,fs) -> f)) topOrderAux
